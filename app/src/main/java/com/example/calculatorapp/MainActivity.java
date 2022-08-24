172260653f188d9891cd752e714945cba558266c
NSeidel3468 <90778376+NSeidel3468@users.noreply.github.com>
2022-08-24 11:04:28 -0500

    Finished square button and added try catch block to prevent crashing.

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -17,61 +17,98 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findSum(View view) {
-            EditText number1ET = findViewById(R.id.num1ET);
-            EditText number2ET = findViewById(R.id.num2ET);
-            TextView numberSumTV = findViewById(R.id.resultTV);
 
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView errText = findViewById(R.id.errorText);
+        try{
             int num1 = Integer.parseInt((number1ET.getText().toString()));
             int num2 = Integer.parseInt((number2ET.getText().toString()));
             int sum = num1 + num2;
-
             numberSumTV.setText("" + sum);
+            errText.setText(" ");
+        }
+        catch(Exception e){
+            errText.setText("Must enter a number");
+        }
+
+
+
     }
 
     public void findProduct(View view)  {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView numberProductTv = findViewById(R.id.resultTV);
+        TextView errText = findViewById(R.id.errorText);
+        try {
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int product = num1 * num2;
+            numberProductTv.setText("" + product);
+            errText.setText(" ");
+        }
+        catch(Exception e){
+            errText.setText("Must enter a number");
+        }
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int product = num1 * num2;
 
-        numberSumTV.setText("" + product);
     }
 
     public void findDifference(View view)  {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView numberDiffTV = findViewById(R.id.resultTV);
+        TextView errText = findViewById(R.id.errorText);
+        try {
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int difference = num1 - num2;
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int difference = num1 - num2;
+            numberDiffTV.setText("" + difference);
+            errText.setText(" ");
+        }
+        catch(Exception e){
+            errText.setText("Must enter a number");
+        }
 
-        numberSumTV.setText("" + difference);
     }
 
     public void findQuotient(View view) {
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView numberQuoTV = findViewById(R.id.resultTV);
+        TextView errText = findViewById(R.id.errorText);
+        try{
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int num2 = Integer.parseInt((number2ET.getText().toString()));
+            int quotient = num1/num2;
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int num2 = Integer.parseInt((number2ET.getText().toString()));
-        int quotient = num1/num2;
+            numberQuoTV.setText("" + quotient);
+            errText.setText(" ");
+        }
+        catch(Exception e){
+            errText.setText("Must enter a number");
+        }
 
-        numberSumTV.setText("" + quotient);
     }
 
     public void findSquare(View view){
         EditText number1ET = findViewById(R.id.num1ET);
         EditText number2ET = findViewById(R.id.num2ET);
-        TextView numberSumTV = findViewById(R.id.resultTV);
+        TextView numberSquareTV = findViewById(R.id.resultTV);
+        TextView errText = findViewById(R.id.errorText);
+        try{
+            int num1 = Integer.parseInt((number1ET.getText().toString()));
+            int square = num1 * num1;
+            number2ET.setText("" + num1);
+            numberSquareTV.setText("" + square);
+            errText.setText(" ");
+        }
+        catch(Exception e){
+            errText.setText("Must enter a number");
+        }
 
-        int num1 = Integer.parseInt((number1ET.getText().toString()));
-        int square = num1 * num1;
-        number2ET.setText(num1);
-        numberSumTV.setText("" + square);
     }
 }
